81bc7faa84dac59a14867d51943e71201b20f362
Jonatas Araujo <jonatasaraujo@gmail.com>
2020-11-16 23:11:55 -0300

    verificação email

--- app/src/main/java/com/example/dhmobilecodersdesafio02/view/LoginScreenActivity.kt
+++ app/src/main/java/com/example/dhmobilecodersdesafio02/view/LoginScreenActivity.kt
@@ -5,17 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.dhmobilecodersdesafio02.R
 
 
 class LoginScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
-
-        val email = findViewById<EditText>(R.id.etLoginEmail)
-        val password = findViewById<EditText>(R.id.etLoginPassword)
-
 
         initComponents()
     }
@@ -23,6 +21,8 @@ class LoginScreenActivity : AppCompatActivity() {
 
     private fun initComponents() {
 
+        var email = findViewById<EditText>(R.id.etLoginEmail)
+        var password = findViewById<EditText>(R.id.etLoginPassword)
 
 
         findViewById<Button>(R.id.btLoginRegister).setOnClickListener {
@@ -31,9 +31,22 @@ class LoginScreenActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btLoginLogin).setOnClickListener {
-            val intent = Intent(this, RestauranteScreenActivity::class.java)
-            startActivity(intent)
+            if (email.text.isNullOrEmpty() || password.text.isNullOrEmpty()){
+                Toast.makeText(this, "Campos obrigatórios", Toast.LENGTH_LONG).show()
+            }
+            else if(!verificaEmail(email)) {
+                Toast.makeText(this, "Email Inválido", Toast.LENGTH_LONG).show()
+            } else{
+                val intent = Intent(this, RestauranteScreenActivity::class.java)
+                startActivity(intent)
+            }
         }
 
     }
+
+
+    private fun verificaEmail(email: EditText): Boolean{
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email.text).matches()
+    }
+
 }
