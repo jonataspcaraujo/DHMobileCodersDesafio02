053e25b75f6c6c015ac9de059c850649b4eddfe6
Jonatas Araujo <jonatasaraujo@gmail.com>
2020-11-16 22:52:29 -0300

    Detalhe Prato

--- app/src/main/java/com/example/dhmobilecodersdesafio02/view/PratoAdapter.kt
+++ app/src/main/java/com/example/dhmobilecodersdesafio02/view/PratoAdapter.kt
@@ -8,26 +8,33 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.dhmobilecodersdesafio02.viewModel.PratoViewHolder
 import com.example.dhmobilecodersdesafio02.model.Prato
 import com.example.dhmobilecodersdesafio02.R
-import com.example.dhmobilecodersdesafio02.viewModel.RestauranteViewHolder
 import com.squareup.picasso.Picasso
 
 class PratoAdapter(
     private val itemList: List<Prato>,
-    private val itView: PratoScreenActivity
+    private val pratoView: PratoScreenActivity
     ):RecyclerView.Adapter<PratoViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PratoViewHolder {
         val view= LayoutInflater.from(parent.context).inflate(R.layout.prato_list_menu,parent,false)
         return PratoViewHolder(view)
     }
 
-    override fun onBindViewHolder(holderHome: PratoViewHolder, position: Int) {
-        Picasso.get().load(itemList[position].itemPicture).into((holderHome.imgView))
-        holderHome.txtItemTitle.text = itemList[position].itemTitle
+    override fun onBindViewHolder(holderPrato: PratoViewHolder, position: Int) {
+        Picasso.get().load(itemList[position].itemPicture).into((holderPrato.imgView))
+        holderPrato.txtItemTitle.text = itemList[position].itemTitle
 
-        holderHome.imgView.setOnClickListener(View.OnClickListener {
-            val intent = Intent(itView, DetalheScreenActivity::class.java)
-            itView.startActivity(intent)
+//        holderPrato.imgView.setOnClickListener(View.OnClickListener {
+//            val intent = Intent(pratoView, DetalheScreenActivity::class.java)
+//            pratoView.startActivity(intent)
+//        })
+        holderPrato.imgView.setOnClickListener(View.OnClickListener {
+            val intent = Intent(pratoView, DetalheScreenActivity::class.java)
+            intent.putExtra("prato",itemList[position])
+            pratoView.startActivity(intent)
         })
+
+
+
     }
 
     override fun getItemCount(): Int {
